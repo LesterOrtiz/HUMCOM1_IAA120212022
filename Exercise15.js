b4d66d6b306936a7450b878d7f669d478799ac47
LesterOrtiz <98294009+LesterOrtiz@users.noreply.github.com>
2022-05-09 23:11:12 +0800

    Add files via upload

---
+++ Exercise15.js
@@ -0,0 +1,4 @@
+document.getElementById("1").innerHTML="STUDENT INFORMATION SHEET";
+document.getElementById("2").innerHTML="PERSONAL INFO";
+document.getElementById("3").innerHTML="CONTACT INFORMATION";
+document.getElementById("4").innerHTML="ACADEMIC INFORMATION";
